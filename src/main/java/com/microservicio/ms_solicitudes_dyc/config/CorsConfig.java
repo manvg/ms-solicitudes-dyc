bdeca4c3d10b01da7f77db74620d96e7abb5242b
Jayirong <lemonsoft5555@gmail.com>
2025-07-13 18:41:37 -0400

    permitir en cors peticiones desde  cortelaserdyc.cl

--- src/main/java/com/microservicio/ms_solicitudes_dyc/config/CorsConfig.java
+++ src/main/java/com/microservicio/ms_solicitudes_dyc/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:4200")
+                    .allowedOrigins("http://localhost:4200", "https://cortelaserdyc.cl", "https://uwwdb8o872.execute-api.us-east-1.amazonaws.com")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*");
             }
